cfc8452a38a96c826e79e66a07a4ed4c19b5b7fa
CarlosLastre <clastres@ups.com>
2021-02-23 18:36:33 +0000

    Proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -8,23 +8,59 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250))
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    name_planetas_id = Column(Integer, ForeignKey('planetas.id'))
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
+   
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    heigth = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+   
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    heigth = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    correo = Column(String(250), nullable=False)
+    passwordr = Column(String(250), nullable=False)
+   
+
+
 
     def to_dict(self):
         return {}
